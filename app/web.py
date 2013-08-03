aef9cf75963c38baa481e118369b018a87dbc5f4
Justyna Ilczuk <justyna.ilczuk@gmail.com>
2013-08-03 12:48:10 +0200

    HS-kasownik-2, HS-kasownik-3
    User can authenticate by auth api.
    Data about user payments and HS cash status is obtained via api and shown in
    /payments view.

--- app/web.py
+++ app/web.py
@@ -2,12 +2,12 @@ from flask import (Flask, render_template, abort,
                    session, request, flash, redirect,
                    url_for)
 
+import requests
+from kasownik_client import APIClient, APIClientException
 
 SECRET_KEY = 'development key'
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-
 
 
 @app.route('/')
@@ -19,15 +19,20 @@ def greet():
 def login():
     error = None
     if request.method == 'POST':
-        if True:
-            username = request.form['username']
-            password = request.form['password']
+        username = request.form['username']
+        password = request.form['password']
+        login_credentials = {"login": username,
+                             "password": password}
+        auth_website = 'https://auth.hackerspace.pl'
+        r = requests.post(auth_website, login_credentials)
+        if r.status_code == 200:
             session['logged_in'] = True
+            session['username'] = username
             flash('You were logged in')
             return redirect(url_for('show_payments'))
-            print password, username
         else:
-            error = "I don't like you"
+            flash('Authorization error,'
+                  ' login or password incorrect!')
 
     return render_template('login.html', error=error)
 
@@ -42,8 +47,13 @@ def logout():
 @app.route('/payments')
 def show_payments():
     if not session.get('logged_in'):
-        abort(401)
-    return render_template('payments.html')
+        redirect(url_for('login'))
+    kasownik = APIClient()
+    mana = kasownik.mana()
+    username = session['username']
+    user_payments_data = kasownik.member_info(member=username)
+    return render_template('payments.html', mana=mana,
+                            payments=user_payments_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
